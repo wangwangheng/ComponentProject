d396fce17b4c94d967f10ddaa4edb7dd274ff066
wangheng <wangheng@mobimagic.com>
2019-03-20 15:54:24 +0800

    AppApplication

---
+++ app/src/main/java/com/xinye/component/AppApplication.kt
@@ -0,0 +1,20 @@
+package com.xinye.component
+
+import android.content.Context
+import com.xinye.core.BaseApplication
+
+/**
+ * 真正的Application对象
+ *
+ * @author wangheng
+ */
+class AppApplication: BaseApplication() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+    }
+}
