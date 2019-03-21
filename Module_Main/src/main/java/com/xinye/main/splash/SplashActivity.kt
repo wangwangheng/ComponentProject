50f102f04d1e4a722e090e1394c391c2f570e449
wangheng <wangheng@mobimagic.com>
2019-03-21 14:43:00 +0800

    签名keystore配置

--- Module_Main/src/main/java/com/xinye/main/splash/SplashActivity.kt
+++ Module_Main/src/main/java/com/xinye/main/splash/SplashActivity.kt
@@ -1,9 +1,15 @@
-package com.xinye.main
+package com.xinye.main.splash
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.xinye.main.R
 import kotlinx.android.synthetic.main.main_activity_splash.*
 
+/**
+ * 闪屏页
+ *
+ * @author wangheng
+ */
 class SplashActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
